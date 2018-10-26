64a1714adb553c98d41761a9534378d40e6beeda
Tim Schieck <tim@jacensolo.com>
2018-10-26 09:30:05 -0500

    Started to add navigation.

--- index.php
+++ index.php
@@ -6,6 +6,8 @@ HeaderStyle("Dashboard");
 <div class="row">
   <div class="col">
     <h1 class="display-1">The Reporter</h1>
+    <a class="btn btn-primary" href="lists.php">Lists</a>
+    <a class="btn btn-primary" href="reports.php">Reports</a>
   </div>
 </div>
 <? FooterStyle() ?>
