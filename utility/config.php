5e882ce19df00d7d57b2e54d380e5468a9ea5546
Tim Schieck <tim@jacensolo.com>
2018-10-26 09:52:13 -0500

    Added new things to hide

--- utility/config.php
+++ utility/config.php
@@ -3,4 +3,12 @@ error_reporting(-1);
 setlocale(LC_MONETARY, "en_US");
 date_default_timezone_set("America/Chicago");
 session_start();
+
+function Authentication() {
+  session_start();
+  if (!isset($_SESSION["user_id"])) {
+    Header("Location:login.php");
+  }
+}
+
 ?>
